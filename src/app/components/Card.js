a45f87b6dda21521de66f132eae204ca2e86aff9
Huan Huffman <huanhuffman@Huans-iMac.local>
2023-11-13 19:48:35 -0800

    Add supabase database integration

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -5,11 +5,11 @@ export default function Card({
   img = 'https://people.com/thmb/Mt39TZIznfUd6jw54_JxCZI8Cd8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(734x164:736x166)/ED-SHEERAN-album-092923-1-70e3c2d64c614c07a301edf7ba1657fc.jpg',
 }) {
   return (
-    <div class="border-solid border border-gray-200 shadow-lg bg-gray-100 text-gray-900 rounded-xl transition-transform hover:scale-[1.05] cursor-pointer grow w-full sm:w-1/2 md:w-1/4">
-      <img class="w-full" src={img} />
-      <h1 class="font-bold text-xl px-5 pt-4 pb-2 underline cursor-pointer">{title}</h1>
-      <h2 class="text-lg px-5">{subtitle}</h2>
-      <p class="text-base px-5 py-2">{description}</p>
+    <div className="border-solid border border-gray-500 shadow-lg bg-gray-100 text-gray-900 rounded-xl transition-transform hover:scale-[1.05] cursor-pointer grow w-full sm:w-1/2 md:w-1/4">
+      <img className="w-full rounded-xl" src={img} />
+      <h1 className="font-bold text-xl px-5 pt-4 pb-2 underline cursor-pointer">{title}</h1>
+      <h2 className="text-lg px-5">{subtitle}</h2>
+      <p className="text-base px-5 py-2">{description}</p>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,26 @@ import PageTitle from './components/PageTitle'
 import PageContent from './components/PageContent'
 import Card from './components/Card'
 
-export default function Home() {
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://mzgowbeebyxaznsntkxx.supabase.co', process.env.SUPABASE_SECRET)
+
+export const revalidate = 0
+
+export default async function Home() {
+  const { data: cards, error } = await supabase.from('cards').select('*')
+
   return (
     <div>
       <Navbar />
       <div className="m-12">
         <PageTitle title="Home" />
         <PageContent content="North State's Premier Strongman Training Center" />
-        <div class="flex flex-wrap w-full gap-6 my-6">
-          <Card title="Our Story" subtitle="Subtitle" description="description" />
-          <Card title="Schedule" subtitle="Subtitle" description="description" />
-          <Card title="Sign Up Now" subtitle="Subtitle" description="description" />
+        <div className="flex flex-wrap w-full gap-6 my-6">
+          {cards.map((card, idx) => (
+            <Card key={idx} title={card.title} subtitle={card.subtitle} description={card.description} img={card.img} />
+          ))}
         </div>
         <PageContent content="North State's Premier Strongman Training Center" />
       </div>
