1210429e24387df1abcca3505c95298ff5bf8bfb
Huan Huffman <huanhuffman@Huans-iMac.local>
2023-11-20 19:58:38 -0800

    Add calendar to schedule page

--- src/app/schedule/page.js
+++ src/app/schedule/page.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
 import PageContent from '../components/PageContent'
 import Footer from '../components/Footer'
+import Script from 'next/script'
 
 export default function Schedule() {
   return (
@@ -11,6 +12,12 @@ export default function Schedule() {
       <div className="m-12">
         <PageTitle title="Schedule" />
         <PageContent content="schedule and events" />
+        <div
+          className="calendly-inline-widget"
+          data-url="https://calendly.com/ironhaus/sundaystrongman"
+          style={{ minWidth: '320px', height: '700px' }}
+        ></div>
+        <Script src="https://assets.calendly.com/assets/external/widget.js" strategy="afterInteractive" />
       </div>
       <Footer />
     </div>
